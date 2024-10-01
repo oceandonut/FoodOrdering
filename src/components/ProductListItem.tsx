cefa191eeef1fa0b36199bbdc9873110d39b1032
oceandonut <rachel99mcwhirter@gmail.com>
2024-10-01 02:58:11 -0700

    7. Expo Router screens

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { Link } from 'expo-router';
 
 import Colors from '@/src/constants/Colors';
 import { Product } from '@/src/types';
@@ -12,13 +13,16 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} 
-        source={{ uri: product.image || defaultPizzaImage }}
-        resizeMode='contain'/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image style={styles.image} 
+          source={{ uri: product.image || defaultPizzaImage }}
+          resizeMode='contain'/>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+
+      </Pressable>
+    </Link>
   );
 }
 
